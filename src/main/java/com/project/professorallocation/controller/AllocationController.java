9c40856d1fca10b4f885120e1098b941f2de82f3
BiaOlimpio <annebeatrizolimpio@hotmail.com>
2022-02-09 19:31:55 -0300

    Add classe de controle

--- src/main/java/com/project/professorallocation/controller/AllocationController.java
+++ src/main/java/com/project/professorallocation/controller/AllocationController.java
@@ -1,5 +1,14 @@
 package com.project.professorallocation.controller;
 
+import com.project.professorallocation.service.AllocationService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AllocationController {
-    
+
+    private AllocationService allocationService;
+
+    public AllocationController(AllocationService allocationService) {
+        this.allocationService = allocationService;
+    }
 }
