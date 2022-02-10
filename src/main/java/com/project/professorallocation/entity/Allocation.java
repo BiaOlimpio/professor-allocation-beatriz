16269dec4d9f3a8133b67c3b48ed7628561ed602
BiaOlimpio <annebeatrizolimpio@hotmail.com>
2022-02-10 20:16:21 -0300

    Add documentação

--- src/main/java/com/project/professorallocation/entity/Allocation.java
+++ src/main/java/com/project/professorallocation/entity/Allocation.java
@@ -1,10 +1,12 @@
 package com.project.professorallocation.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
 import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.time.DayOfWeek;
@@ -15,6 +17,7 @@ import java.util.Date;
 public class Allocation {
 
     @Id
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -22,6 +25,8 @@ public class Allocation {
     @Column(nullable = false)
     private DayOfWeek day;
 
+
+    @ApiModelProperty(example = "12:00-0300")
     @JsonFormat(pattern = "HH:mmZ")
     @JsonSerialize(using = DateSerializer.class)
     @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
@@ -29,25 +34,29 @@ public class Allocation {
     private Date start;
     @Temporal (TemporalType.TIME)
 
+    @ApiModelProperty(example = "16:00-0300")
     @JsonFormat(pattern = "HH:mmZ")
     @JsonSerialize(using = DateSerializer.class)
     @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
     @Column(nullable = false)
     private Date end;
 
-
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "course_id" , nullable = false)
     private Long courseId;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "professor_id" , nullable = false)
     private Long professorId;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @ManyToOne(optional = false)
     @JoinColumn(name =  "course_id" , nullable = false, insertable = false, updatable = false)
     private Course course;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @ManyToOne(optional = false)
-@JoinColumn(name = "professor_id" , nullable = false, insertable = false, updatable = false)
+    @JoinColumn(name = "professor_id" , nullable = false, insertable = false, updatable = false)
     private Professor professor;
 
 
